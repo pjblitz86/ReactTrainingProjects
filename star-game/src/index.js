11dfcb50460daa5a465d1518897eca333b01c6c4
Paulius Juknis <paulius.juknis@gmail.com>
2018-09-28 11:19:10 +0300

    set up Button and Result component and wrapped them in App for render

--- star-game/src/index.js
+++ star-game/src/index.js
@@ -1,8 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+
+// class component with state
+class Button extends Component {
+  state = { counter: 0 };
+
+  handleClick = () => {
+    this.setState(prevState => ({
+      counter: prevState.counter + 1
+    }));
+  };
+
+  render() {
+    return <button onClick={this.handleClick}>{this.state.counter}</button>;
+  }
+}
+
+// function component
+const Result = props => {
+  return <div>...</div>;
+};
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Button />
+        <Result />
+      </div>
+    );
+  }
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
