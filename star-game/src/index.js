eee75fcc12a1d0fda727e1d51a40502c535f39ec
Paulius Juknis <paulius.juknis@gmail.com>
2018-09-28 13:16:27 +0300

    implemented onclickbutton functionality

--- star-game/src/index.js
+++ star-game/src/index.js
@@ -4,30 +4,37 @@ import './index.css';
 
 // class component with state
 class Button extends Component {
-  state = { counter: 0 };
-
   handleClick = () => {
-    this.setState(prevState => ({
-      counter: prevState.counter + 1
-    }));
+    this.props.onClickFunction(this.props.incrementValue);
   };
 
   render() {
-    return <button onClick={this.handleClick}>{this.state.counter}</button>;
+    return <button onClick={this.handleClick}>+{this.props.incrementValue}</button>;
   }
 }
 
 // function component
 const Result = props => {
-  return <div>...</div>;
+  return <div>{props.counter}</div>;
 };
 
+// main component on which all are mounted
 class App extends Component {
+  state = { counter: 0 };
+  incrementCounter = incrementValue => {
+    this.setState(prevState => ({
+      counter: prevState.counter + incrementValue
+    }));
+  };
+
   render() {
     return (
       <div>
-        <Button />
-        <Result />
+        <Button incrementValue={1} onClickFunction={this.incrementCounter} />
+        <Button incrementValue={5} onClickFunction={this.incrementCounter} />
+        <Button incrementValue={20} onClickFunction={this.incrementCounter} />
+        <Button incrementValue={100} onClickFunction={this.incrementCounter} />
+        <Result counter={this.state.counter} />
       </div>
     );
   }
