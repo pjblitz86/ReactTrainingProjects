51bfc0009aa7b695b98a3c95f7ee0a794b48b401
pjblitz86 <paulius.juknis@gmail.com>
2018-09-29 19:06:14 +0300

    adding a functional/stateless component example

--- udemy-max-tutorials/src/App.js
+++ udemy-max-tutorials/src/App.js
@@ -1,13 +1,22 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>Hi, I'm react app</h1>
+        <p>this is really working</p>
+        <Person />
       </div>
     );
+    // other way - this is under the hood
+    // return React.createElement(
+    //   'div',
+    //   { className: 'App' },
+    //   React.createElement('h1', null, 'does this work')
+    // );
   }
 }
 
---
+++ udemy-max-tutorials/src/Person/Person.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+// functional component example with ES6 best practice
+const person = () => {
+  return <p>I'm a person</p>;
+};
+
+export default person;
