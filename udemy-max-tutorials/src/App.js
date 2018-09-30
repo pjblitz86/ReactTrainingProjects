0e5ee29d9f4c2a48d29b1ea34f1549c2b9531b5e
pjblitz86 <paulius.juknis@gmail.com>
2018-09-30 12:02:09 +0300

    started using state property in a class

--- udemy-max-tutorials/src/App.js
+++ udemy-max-tutorials/src/App.js
@@ -3,13 +3,17 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+  state = {
+    persons: [{ name: 'PJ', age: 24 }, { name: 'Jonas', age: 25 }]
+  };
   render() {
     return (
       <div className="App">
         <h1>Hi, I'm react app</h1>
         <p>this is really working</p>
-        <Person name="PJ" age="28" />
-        <Person name="Jonas" age="24">
+        <button>Switch Name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>
           My Hobbies: racing
         </Person>
       </div>
