0d5a0cc2a1a642fdd23292d202e1792535131c78
Paulius Juknis <paulius.juknis@gmail.com>
2018-10-02 14:59:26 +0300

    implemented map on array

--- udemy-max-tutorials/lists-conditionals--assignment-problem/src/App.js
+++ udemy-max-tutorials/lists-conditionals--assignment-problem/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import Validation from './Validation/Validation';
+import Char from './Char/Char';
 
 class App extends Component {
   state = {
@@ -12,6 +13,10 @@ class App extends Component {
   };
 
   render() {
+    let charList = this.state.userInput.split('').map((ch, index) => {
+      return <Char character={ch} key={index} />;
+    });
+
     return (
       <div className="App">
         <ol>
@@ -42,6 +47,7 @@ class App extends Component {
         <input type="text" onChange={this.inputChangegHandler} value={this.state.userInput} />
         <p>{this.state.userInput}</p>
         <Validation inputLength={this.state.userInput.length} />
+        {charList}
       </div>
     );
   }
