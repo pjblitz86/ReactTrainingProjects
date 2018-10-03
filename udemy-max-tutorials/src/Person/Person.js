15fda4562d47bdb5a955dd9d5ebe4459b0969a5d
Paulius Juknis <paulius.juknis@gmail.com>
2018-10-03 11:16:30 +0300

    enabling css modules by ejecting npm configuration

--- udemy-max-tutorials/src/Person/Person.js
+++ udemy-max-tutorials/src/Person/Person.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import './Person.css';
+import classes from './Person.css';
 
 // functional component example with ES6 best practice
 const person = props => {
   return (
-    <div className="Person">
+    <div className={classes.Person}>
       <p onClick={props.click}>
         I'm a {props.name} and i am {props.age} years old
       </p>
