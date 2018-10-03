924a8c59960b2314eaeb18cd53946eda01ba1bd6
Paulius Juknis <paulius.juknis@gmail.com>
2018-10-03 09:06:29 +0300

    cleaning up default created app

--- lyric-search-app/src/App.js
+++ lyric-search-app/src/App.js
@@ -1,9 +1,23 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
+import Navbar from './components/layout/Navbar';
+import Index from './components/layout/Index';
 
 class App extends Component {
   render() {
-    return <h1>Hello</h1>;
+    return (
+      <Router>
+        <React.Fragment>
+          <Navbar />
+          <div className="container">
+            <Switch>
+              <Route exact path="/" component={Index} />
+            </Switch>
+          </div>
+        </React.Fragment>
+      </Router>
+    );
   }
 }
 
