c36f467d7f6906c6ea22666a5098ac57942ea376
Paulius Juknis <paulius.juknis@gmail.com>
2018-10-03 13:56:43 +0300

    adding errorboundary for capturing errors

--- udemy-max-tutorials/src/App.js
+++ udemy-max-tutorials/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import classes from './App.css';
 import Person from './Person/Person';
+import ErrorBoundary from './ErrorBoundary/ErrorBoundary';
 
 class App extends Component {
   state = {
@@ -10,7 +11,7 @@ class App extends Component {
   };
 
   nameChangeHandler = (event, id) => {
-    const personIndex = this.state.persons.find(p => {
+    const personIndex = this.state.persons.findIndex(p => {
       return p.id === id;
     });
 
@@ -46,13 +47,16 @@ class App extends Component {
         <div>
           {this.state.persons.map((person, index) => {
             return (
-              <Person
-                click={() => this.deletePersonHandler(index)}
-                name={person.name}
-                age={person.age}
-                key={person.id} // usually id - unique
-                changed={event => this.nameChangeHandler(event, person.id)}
-              />
+              <ErrorBoundary>
+                key=
+                {person.id}
+                <Person
+                  click={() => this.deletePersonHandler(index)}
+                  name={person.name}
+                  age={person.age}
+                  changed={event => this.nameChangeHandler(event, person.id)}
+                />
+              </ErrorBoundary>
             );
           })}
         </div>
