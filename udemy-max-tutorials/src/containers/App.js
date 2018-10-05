e389b54fb18f314a9584bf9ba2c349471af41799
Paulius Juknis <paulius.juknis@gmail.com>
2018-10-05 12:08:45 +0300

    updated lifecycle hooks - added 2 new from 16.3 React version

--- udemy-max-tutorials/src/containers/App.js
+++ udemy-max-tutorials/src/containers/App.js
@@ -44,6 +44,20 @@ class App extends PureComponent {
     );
   }
 
+  // New lifecycle from React 16.3
+  static getDerivedStateFromProps(nextProps, prevState) {
+    console.log(
+      '[UPDATE App.js] Inside getDerivedStateFromProps',
+      nextProps,
+      prevState
+    );
+    return prevState;
+  }
+
+  getSnapshotBeforeUpdate() {
+    console.log('[UPDATE App.js] Inside getSnapshotBeforeUpdate');
+  }
+
   componentDidUpdate() {
     console.log('[UPDATE App.js] Inside componentDidUpdate');
   }
