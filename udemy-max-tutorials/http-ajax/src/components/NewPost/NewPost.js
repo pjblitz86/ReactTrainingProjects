6f9907746c099459cb46434a9a3b4f17268f8012
Paulius Juknis <paulius.juknis@gmail.com>
2018-12-18 14:48:49 +0200

    setting global axios configuration

--- udemy-max-tutorials/http-ajax/src/components/NewPost/NewPost.js
+++ udemy-max-tutorials/http-ajax/src/components/NewPost/NewPost.js
@@ -16,7 +16,7 @@ class NewPost extends Component {
       body: this.state.body,
       author: this.state.author
     };
-    axios.post("https://jsonplaceholder.typicode.com/posts", data).then(res => {
+    axios.post("/posts", data).then(res => {
       console.log(res);
     });
   };
