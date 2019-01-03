cb6851b653b7f592942a4c9c0a464b9e78a49c5b
Paulius Juknis <paulius.juknis@gmail.com>
2019-01-03 11:03:01 +0200

    update array immutably

--- udemy-max-tutorials/redux-basics/src/store/reducer.js
+++ udemy-max-tutorials/redux-basics/src/store/reducer.js
@@ -29,6 +29,17 @@ const reducer = (state = initialState, action) => {
         ...state,
         results: state.results.concat({ id: new Date(), value: state.counter }) // dont use push we must update immutably
       };
+    case "DELETE_RESULT":
+      // const id = 2;
+      // const newArray = [...state.results];
+      // newArray.splice(id,1) 1way of doing delete
+      const updatedArray = state.results.filter(
+        result => result.id !== action.resultElId
+      ); // 2nd way for delete
+      return {
+        ...state,
+        results: updatedArray
+      };
     default:
       return state;
   }
