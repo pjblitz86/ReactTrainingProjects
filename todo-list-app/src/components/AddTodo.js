9e1f158fd72c7ba849f814dc7a9b3a362708611b
Paulius Juknis <paulius.juknis@gmail.com>
2019-01-04 14:18:07 +0200

    AddTodo component

---
+++ todo-list-app/src/components/AddTodo.js
@@ -0,0 +1,32 @@
+import React, { Component } from "react";
+
+export class AddTodo extends Component {
+  state = {
+    title: ""
+  };
+
+  onChange = e => this.setState({ [e.target.name]: e.target.value });
+
+  render() {
+    return (
+      <form style={{ display: "flex" }}>
+        <input
+          type="text"
+          name="title"
+          placeholder="Add Todo ..."
+          style={{ flex: "10", padding: "5px" }}
+          value={this.state.title}
+          onChange={this.onChange}
+        />
+        <input
+          type="submit"
+          value="submit"
+          className="btn"
+          style={{ flex: "1" }}
+        />
+      </form>
+    );
+  }
+}
+
+export default AddTodo;
