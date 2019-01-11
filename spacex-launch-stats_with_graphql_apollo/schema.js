cad8520ce0e0dbdb32983c5940b92f8caa387a03
Paulius Juknis <paulius.juknis@gmail.com>
2019-01-11 16:12:55 +0200

    set-up graphql object types

--- spacex-launch-stats_with_graphql_apollo/schema.js
+++ spacex-launch-stats_with_graphql_apollo/schema.js
@@ -1,8 +1,12 @@
+const axios = require("axios");
+
 const {
   GraphQLObjectType,
   GraphQLInt,
   GraphQLString,
-  GraphQLBoolean
+  GraphQLBoolean,
+  GraphQLList,
+  GraphQLSchema
 } = require("graphql");
 
 const LaunchType = new GraphQLObjectType({
@@ -24,4 +28,22 @@ const RocketType = new GraphQLObjectType({
     rocket_name: { type: GraphQLString },
     rocket_type: { type: GraphQLString }
   })
+});
+
+const RootQuery = new GraphQLObjectType({
+  name: "RootQueryType",
+  fields: {
+    launches: {
+      type: new GraphQLList(LaunchType),
+      resolve(parent, args) {
+        return axios
+          .get("https://api.spacexdata.com/v3/launches")
+          .then(res => res.data);
+      }
+    }
+  }
+});
+
+module.exports = new GraphQLSchema({
+  query: RootQuery
 });
