923295f2cd66fce8062e74687c9e7c51f49ded28
Paulius Juknis <paulius.juknis@gmail.com>
2019-01-24 11:23:52 +0200

    fetching images from api implemented

--- pixabay-img-finder-material-ui/src/components/ImageResults/ImageResults.js
+++ pixabay-img-finder-material-ui/src/components/ImageResults/ImageResults.js
@@ -4,33 +4,41 @@ import GridList from "@material-ui/core/GridList";
 import GridListTile from "@material-ui/core/GridListTile";
 import IconButton from "@material-ui/core/IconButton";
 import InfoIcon from "@material-ui/icons/Info";
+import GridListTileBar from "@material-ui/core/GridListTileBar";
+import { withStyles } from "@material-ui/core/styles";
 
 // ZoomIn, FlatButton
 import Dialog from "@material-ui/core/Dialog";
 
+const styles = () => ({
+  icon: {
+    color: "rgba(255, 255, 255, 0.54)"
+  }
+});
+
 class ImageResults extends Component {
   render() {
     let imageListContent;
-    const { images } = this.props;
+    const { images, classes } = this.props;
     if (images) {
       imageListContent = (
         <GridList cellHeight={180} cols={3}>
           {images.map(img => (
-            <GridListTile
-              title={img.tags}
-              key={img.id}
-              subtitle={
-                <span>
-                  by <strong>{img.user}</strong>
-                </span>
-              }
-              actionIcon={
-                <IconButton>
-                  <InfoIcon color="white" />
-                </IconButton>
-              }
-            >
+            <GridListTile key={img.id}>
               <img src={img.largeImageURL} alt="" />
+              <GridListTileBar
+                title={img.tags}
+                subtitle={
+                  <span>
+                    by <strong>{img.user}</strong>
+                  </span>
+                }
+                actionIcon={
+                  <IconButton className={classes.icon}>
+                    <InfoIcon />
+                  </IconButton>
+                }
+              />
             </GridListTile>
           ))}
         </GridList>
@@ -39,7 +47,7 @@ class ImageResults extends Component {
       imageListContent = null;
     }
 
-    return <div />;
+    return <div>{imageListContent}</div>;
   }
 }
 
@@ -47,4 +55,4 @@ ImageResults.propTypes = {
   images: PropTypes.array.isRequired
 };
 
-export default ImageResults;
+export default withStyles(styles)(ImageResults);
--- pixabay-img-finder-material-ui/src/components/Navbar/Navbar.js
+++ pixabay-img-finder-material-ui/src/components/Navbar/Navbar.js
@@ -38,6 +38,7 @@ function NavBar(props) {
           </Typography>
         </Toolbar>
       </AppBar>
+      <br />
     </div>
   );
 }
