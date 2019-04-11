97775125baed82f9b7327702ed7514936ae95bae
Paulius Juknis <paulius.juknis@gmail.com>
2019-04-11 13:59:46 +0300

    progress bar implementation

--- file-uploader/client/src/components/Message.js
+++ file-uploader/client/src/components/Message.js
@@ -3,10 +3,7 @@ import PropTypes from "prop-types";
 
 const Message = ({ msg }) => {
   return (
-    <div
-      className="alert alert-warning alert-dismissible fade show"
-      role="alert"
-    >
+    <div className="alert alert-info alert-dismissible fade show" role="alert">
       {msg}
       <button
         type="button"
