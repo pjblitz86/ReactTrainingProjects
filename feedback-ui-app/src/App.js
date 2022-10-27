dd1f18f57fb0692ccce0ecb011a2384e08784fdb
pjblitz86 <paulius.juknis@gmail.com>
2022-10-27 10:48:57 +0300

    state and usestate hook

--- feedback-ui-app/src/App.js
+++ feedback-ui-app/src/App.js
@@ -1,11 +1,12 @@
 import Header from './components/Header';
+import FeedBackItem from './components/FeedBackItem';
 
 function App() {
   return (
     <>
       <Header />
       <div className='container'>
-        <h1>My app</h1>
+        <FeedBackItem />
       </div>
     </>
   );
