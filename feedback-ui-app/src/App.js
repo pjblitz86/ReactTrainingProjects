3a024c1ed1112f2419b1ed99ce289cfb8858e204
pjblitz86 <paulius.juknis@gmail.com>
2022-10-31 09:40:16 +0200

    prop drilling for feedback item delete

--- feedback-ui-app/src/App.js
+++ feedback-ui-app/src/App.js
@@ -6,11 +6,17 @@ import FeedBackData from './data/FeedbackData';
 function App() {
   const [feedback, setFeedback] = useState(FeedBackData);
 
+  const deleteFeedback = (id) => {
+    if (window.confirm('Are you sure you want to delete?')) {
+      setFeedback(feedback.filter((item) => item.id !== id));
+    }
+  };
+
   return (
     <>
       <Header />
       <div className='container'>
-        <FeedBackList feedback={feedback} />
+        <FeedBackList feedback={feedback} handleDelete={deleteFeedback} />
       </div>
     </>
   );
