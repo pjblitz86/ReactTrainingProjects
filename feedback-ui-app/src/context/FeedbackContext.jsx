d468db1109d74d85acc032d406351126ff88f81c
pjblitz86 <paulius.juknis@gmail.com>
2022-11-04 11:07:36 +0200

    update item implementation

--- feedback-ui-app/src/context/FeedbackContext.jsx
+++ feedback-ui-app/src/context/FeedbackContext.jsx
@@ -45,14 +45,21 @@ export const FeedbackProvider = ({ children }) => {
     });
   };
 
+  const updateFeedback = (id, updItem) => {
+    setFeedback(
+      feedback.map((item) => (item.id === id ? { ...item, ...updItem } : item))
+    );
+  };
+
   return (
     <FeedbackContext.Provider
       value={{
         feedback,
+        feedbackEdit,
         deleteFeedback,
         addFeedback,
         editFeedback,
-        feedbackEdit
+        updateFeedback
       }}
     >
       {children}
