9ca70b20161552cf03a5829c6db48390f52eeb9e
pjblitz86 <paulius.juknis@gmail.com>
2022-11-04 10:29:46 +0200

    editfeedback event in context

--- feedback-ui-app/src/context/FeedbackContext.jsx
+++ feedback-ui-app/src/context/FeedbackContext.jsx
@@ -7,10 +7,25 @@ export const FeedbackProvider = ({ children }) => {
   const [feedback, setFeedback] = useState([
     {
       id: 1,
-      text: 'This item is from context',
+      text: 'This is item 1',
       rating: 10
+    },
+    {
+      id: 2,
+      text: 'This is item 2',
+      rating: 9
+    },
+    {
+      id: 3,
+      text: 'This is item 3',
+      rating: 8
     }
   ]);
+
+  const [feedbackEdit, setFeedbackEdit] = useState({
+    item: {},
+    edit: false
+  });
 
   const addFeedback = (newFeedback) => {
     newFeedback.id = uuidv4();
@@ -23,12 +38,20 @@ export const FeedbackProvider = ({ children }) => {
     }
   };
 
+  const editFeedback = (item) => {
+    setFeedbackEdit({
+      item,
+      edit: true
+    });
+  };
+
   return (
     <FeedbackContext.Provider
       value={{
         feedback,
         deleteFeedback,
-        addFeedback
+        addFeedback,
+        editFeedback
       }}
     >
       {children}
