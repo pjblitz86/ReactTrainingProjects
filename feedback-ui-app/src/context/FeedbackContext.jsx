2324bcf81d1c9bae241a7c95400e5f8ce98479d0
pjblitz86 <paulius.juknis@gmail.com>
2022-11-04 09:49:37 +0200

    refactor to global state with useContext hook

--- feedback-ui-app/src/context/FeedbackContext.jsx
+++ feedback-ui-app/src/context/FeedbackContext.jsx
@@ -3,13 +3,13 @@ import { createContext, useState } from 'react';
 const FeedbackContext = createContext();
 
 export const FeedbackProvider = ({ children }) => {
-  const [feedback, setFeedback] = [
+  const [feedback, setFeedback] = useState([
     {
       id: 1,
       text: 'This item is from context',
       rating: 10
     }
-  ];
+  ]);
 
   return (
     <FeedbackContext.Provider
