89f5a5479a6e9f2439443793b71da65009c8d368
pjblitz86 <paulius.juknis@gmail.com>
2022-11-09 10:55:50 +0200

    clear users action

--- github-finder/src/context/github/GithubContext.js
+++ github-finder/src/context/github/GithubContext.js
@@ -33,6 +33,8 @@ export const GithubProvider = ({ children }) => {
     });
   };
 
+  const clearUsers = () => dispatch({ type: 'CLEAR_USERS' });
+
   const setLoading = () =>
     dispatch({
       type: 'SET_LOADING'
@@ -40,7 +42,12 @@ export const GithubProvider = ({ children }) => {
 
   return (
     <GithubContext.Provider
-      value={{ users: state.users, loading: state.loading, searchUsers }}
+      value={{
+        users: state.users,
+        loading: state.loading,
+        searchUsers,
+        clearUsers
+      }}
     >
       {children}
     </GithubContext.Provider>
