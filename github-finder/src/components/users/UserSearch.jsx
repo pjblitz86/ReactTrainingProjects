d8f1e5f7ee5d9590fefa7003d0e381ab331518d4
pjblitz86 <paulius.juknis@gmail.com>
2022-11-10 10:23:07 +0200

    alert component

--- github-finder/src/components/users/UserSearch.jsx
+++ github-finder/src/components/users/UserSearch.jsx
@@ -1,17 +1,19 @@
 import { useState, useContext } from 'react';
 import GithubContext from '../../context/github/GithubContext';
+import AlertContext from '../../context/github/alert/AlertContext';
 
 function UserSearch() {
   const [text, setText] = useState('');
 
   const { users, searchUsers, clearUsers } = useContext(GithubContext);
+  const { setAlert } = useContext(AlertContext);
 
   const handleChange = (e) => setText(e.target.value);
 
   const handleSubmit = (e) => {
     e.preventDefault();
     if (text === '') {
-      alert('Please enter smth');
+      setAlert('Please enter smth', 'error');
     } else {
       searchUsers(text);
     }
