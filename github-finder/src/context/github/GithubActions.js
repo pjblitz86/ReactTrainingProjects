31b9c8bb5d561b74ef98326d349b107e00cac06a
pjblitz86 <paulius.juknis@gmail.com>
2022-11-11 11:54:41 +0200

    move getUser and repos to actions file

--- github-finder/src/context/github/GithubActions.js
+++ github-finder/src/context/github/GithubActions.js
@@ -15,3 +15,34 @@ export const searchUsers = async (text) => {
   const { items } = await response.json();
   return items;
 };
+
+export const getUser = async (login) => {
+  const response = await fetch(`${GITHUB_URL}/users/${login}`, {
+    headers: {
+      Authorization: `token ${GITHUB_TOKEN}`
+    }
+  });
+
+  if (response.status === 404) {
+    window.location = '/notfound';
+  } else {
+    const data = await response.json();
+    return data;
+  }
+};
+
+export const getUserRepos = async (login) => {
+  const params = new URLSearchParams({
+    sort: 'created',
+    per_page: 10
+  });
+
+  const response = await fetch(`${GITHUB_URL}/users/${login}/repos?${params}`, {
+    headers: {
+      Authorization: `token ${GITHUB_TOKEN}`
+    }
+  });
+
+  const data = await response.json();
+  return data;
+};
